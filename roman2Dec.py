2d882fe5c601d24da4376e936bd77341dd2209fb
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-07-12 23:10:19 +0530

    Add files via upload

---
+++ roman2Dec.py
@@ -0,0 +1,16 @@
+def roman2Dec(romstr):
+    roman_dict={'I':1 ,'V':5,'X':10,'L':50,'C':100,'D':500, 'M':1000}
+    romanBack=list(romanstr)[::-1]
+    value=0
+    rightVal=roman_dict[romanBack[0]]
+    for numeral in romanBack:
+        leftVal=roman_dict[numeral]
+        if leftVal<rightVal:
+            value-=leftVal
+        else:
+            value+=leftVal
+        rightVal=leftVal
+    return value
+romanstr=input('Enter a roman number')
+print(roman2Dec(romanstr))
+
