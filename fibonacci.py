09a7fc00abd8a97a040e4e11f5d55bcb098e1e27
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-07-25 23:21:49 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+def fn(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fn(n-1)+fn(n-2)
+num=int(input("enter the number"))
+if num>0:
+    print("fn(" ,num, ")=",fn(num),sep="")
+else:
+    print("error in input")
